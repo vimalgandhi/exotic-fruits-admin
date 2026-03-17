bfb636a1b6152705b0a1d4fa5dc256237f82992f
Vims1012 <65559455+vimalgandhi@users.noreply.github.com>
2026-03-17 18:31:51 +0530

    Create page.tsx file to render ProductList component

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductList from './ProductList';
+
+const Page = () => {
+    return (
+        <div>
+            <ProductList />
+        </div>
+    );
+};
+
+export default Page;
