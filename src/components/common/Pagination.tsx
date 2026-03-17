476e0dc1e9f9275924d30acd6b0166f18bacdc32
Vims1012 <65559455+vimalgandhi@users.noreply.github.com>
2026-03-17 17:29:10 +0530

    Add Pagination component

---
+++ src/components/common/Pagination.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="pagination">
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>
+        Previous
+      </button>
+      <span>
+        Page {currentPage} of {totalPages}
+      </span>
+      <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
